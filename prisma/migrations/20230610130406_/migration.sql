365b0a3209de66362a1e56c728c6fd4206507ea6
satokiyoshino <satokiyoshino@gmail.com>
2023-06-10 22:04:20 +0900

    fix

--- prisma/migrations/20230610130406_/migration.sql
+++ prisma/migrations/20230610130406_/migration.sql
@@ -1,13 +1,4 @@
 -- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Role" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
