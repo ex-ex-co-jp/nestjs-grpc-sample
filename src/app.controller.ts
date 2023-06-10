da3a4db8c90c88ae272b8b6261e8a649e74c67eb
satokiyoshino <satokiyoshino@gmail.com>
2023-06-10 16:53:13 +0900

    introduce prisma&postgres

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,18 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { Sample, SampleById } from '@/proto/sample';
+import { AppService } from './app.service';
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
   @GrpcMethod('SamplesService', 'FindOne')
-  findOne(data: SampleById): Sample {
+  async findOne(data: SampleById): Promise<Sample> {
     const items = [
       { id: 1, name: 'John' },
       { id: 2, name: 'Doe' },
     ] as Sample[];
+    const user = await this.appService.user({ id: data.id });
+    console.log({ user });
     const filteredItems = items.filter((item) => item.id === data.id);
     return filteredItems[0];
   }
